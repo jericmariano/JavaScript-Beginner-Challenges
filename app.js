452ace0fd0dd22096cde0007b7dc4d2433a7f8cd
Jeric <marianovjeric@gmail.com>
2022-11-17 21:31:57 -0800

    question 6, greater than 100

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// Question 5, extend a string
+// Question 6, greater than 100?
 
-function appendFrontend (str) {
-    return str + 'Frontend'
+function sumGreaterThan100 (num1, num2) {
+   return num1 + num2 > 100
 }
 
-console.log(appendFrontend('Apple'));
+console.log(sumGreaterThan100(50, 100));
