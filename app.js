cff302332e0668996d54e8187933e0f071896a98
Jeric <marianovjeric@gmail.com>
2022-11-17 21:58:23 -0800

    question 14 check if userr is logged in or subscribed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// Question 13, check if user is logged in and subscribed
+// Question 14, check if user is logged in OR subscribed
 
 function isLoggedInAndSubscribed (bool1, bool2) {
-   return bool1 === 'LOGGED_IN' && bool2 === 'SUBSCRIBED'
+   return bool1 === 'LOGGED_IN' || bool2 === 'SUBSCRIBED'
 }
 
-console.log(isLoggedInAndSubscribed('LOGGED_IN', 'SUBSCRIBED'));
+console.log(isLoggedInAndSubscribed('LOGGED_IN', 'UNSUBSCRIBED'));
